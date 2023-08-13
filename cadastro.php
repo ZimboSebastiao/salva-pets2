88b8db84b95c540f02c028bbc30c0ebb97dcd5cc
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-08-13 14:29:47 -0300

    Estilizando página de cadastro

---
+++ cadastro.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro - Salvapets</title>
+    <!-- ======== CSS Bootstrap ======== -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+</head>
+<body>
+
+    <main>
+        <div class="container">
+            <div class="row">
+
+              <!-- =========== Formulário  =========== -->
+              <div class="col-6">
+                  
+                <div>
+                    <div id="pata" class="d-flex flex-column justify-content-center align-items-center"></div>
+                    <h1>A sua adoção <br> começa aqui</h1>
+
+                    <p class="estilo-p">Faça parte de um mundo mais amigável</p>
+                </div>
+
+                <section class="estilo-form">
+                    <form action=""  method="post">
+
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control input" id="floatingInputGroup2" placeholder="Username" required>
+                            <label for="floatingInputGroup2">Nome completo</label>
+                        </div>
+                        
+                        <div class="form-floating mb-3">
+                            <input class="form-control input" id="floatingInput" placeholder="name@example.com" required type="email">
+                            <label for="floatingInput">E-mail</label>
+                        </div>
+                        
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control input" id="floatingInputGroup2" placeholder="Username" required>
+                            <label for="floatingInputGroup2">CEP</label>
+                        </div>
+
+                        <div class="form-floating mb-3">
+                            <input  class="form-control input" id="floatingPassword" placeholder="Password" required type="password">
+                            <label for="floatingPassword">Senha</label>
+                        </div>
+
+                        <div class="form-floating mb-3">
+                            <input  class="form-control input" id="floatingPassword" placeholder="Password" required type="password">
+                            <label for="floatingPassword">Confirma senha</label>
+                        </div>
+        
+                        <div class="form-check estilo-l">
+                            <input class="form-check-input" type="checkbox" value="" id="" >
+                            <label class="form-check-label" for="" >
+                                Ao clicar no botão "Criar conta", você aceita <br>
+                                nossos termos de uso e politíca de privacidade
+                            </label>
+                        </div>
+
+                        <div>
+                            <button class="btn btn-primary btn-lg botao">Criar Conta</button>
+                            <p class="tag-style-c">Já tem uma conta? <a href="">Login</a></p>
+                        </div>
+
+
+                    </form>
+                </section> 
+              </div>
+
+              <!-- ========== Destaque ========== -->
+              <div class="col-8" id="teste" ></div>
+          </div>
+        </div>
+
+    </main>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+</html>
--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login - salvapets</title>
+    <title>Salvapets - entre ou cadastre-se</title>
     <!-- ======== CSS Bootstrap ======== -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/styles.css">
@@ -64,29 +64,13 @@
           </div>
         </div>
         
-
-
-        <!-- 
-        <section>
-            <form action="">
-                <label for="email">E-mail</label>
-                <input placeholder="Insira seu e-mail" required type="email">
-
-                <label for="senha">Senha</label>
-                <input placeholder="Insira sua senha" required type="password">
-                <a href="" rel="pagina de criar nova senha">Esqueci a senha</a>
-                <button>Entrar</button>
-            </form>
-            <p>Não tem uma conta? <a href="">Crie uma conta de graça</a></p>
-        </section> 
-    
-    -->
+        <?php 
+        
+        
+        ?>
 
     </main>
-    <!-- ======= Hero Section ======= -->
-    <!-- <section id="destaque">
-    </section> -->
-    <!-- End Hero -->
+
     
 
 
