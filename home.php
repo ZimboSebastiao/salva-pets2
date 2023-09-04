2f3a555a38361436739043547c3ee4b05f90e6ce
Nicolas_Amorim <ni.oliveiraamorim.25@gmail.com>
2023-09-04 11:56:27 -0300

    home
    Input de busca

--- home.php
+++ home.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SalvaPets - Home</title>
-    <!-- ======== CSS Bootstrap ======== -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/styles.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SalvaPets - Home</title>
+  <!-- ======== CSS Bootstrap ======== -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/styles.css">
 </head>
+
 <body>
+  <header>
 
-
-    <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+  <!-- Menu Responsivo -->
+    <nav class="navbar navbar-expand-lg  bg-dark static-top">
       <div class="container">
         <a class="navbar-brand" href="#">
-          <img src="icones/pet1.png" alt="..." height="46"> 
+          <img src="icones/pet1.png" alt="..." height="46">
           Salva Pets
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -46,12 +48,45 @@
         </div>
       </div>
     </nav>
+    
+    <!-- Destaque encontre e adote -->
     <div class="cabecalho">
       <h1>Encontre e adote <br>
-      um pet para você.</h1>
-    </div>
-    </header>
+        um pet para você.</h1> 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>    
+      <p>
+        Você não pode comprar amor,
+        mas você pode adotar ele.
+        Adote um pet para fazer companhia🐾
+      </p>
+
+      <!-- Input Cidade -->
+      <div class="input-group mb-2  bg-dark">
+        <img src="icones/pata (1).png" alt="" width="10%">  
+        <input type="text" class="form-control" aria-label="Sizing example input"  aria-describedby="inputGroup-sizing-default"placeholder="Cidade">
+      </div>
+
+      <!-- Input Região -->
+      <div class="input-group mb-2  bg-dark">
+        <img src="icones/pet-house.png" alt="" width="10%">  
+        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Região">
+      </div>
+
+      <!-- Input Animal-->
+      <div class="input-group mb-2  bg-dark">
+        <img src="icones/bola-de-cachorro (1).png" alt="" width="10%">  
+        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Animal">
+      </div>
+
+      <!-- Input Raça -->
+      <div class="input-group mb-2  bg-dark">
+        <img src="icones/bicho-de-estimacao (1).png" alt="" width="10%">  
+        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Raça">
+      </div>
+    </div>
+  </header>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
+
 </html>
