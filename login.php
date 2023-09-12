18162ea7e0de12105803d44489295ec6923853d0
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-12 14:07:08 -0300

    pagina de adoção

--- login.php
+++ login.php
@@ -16,34 +16,34 @@
         <!-- =========== Formulário  =========== -->
         <div class="col-6">
             
-        <div>
-            <div id="pata" class="d-flex flex-column justify-content-center align-items-center"></div>
-            <h1>Seja bem <br> vindo de volta</h1>
-            <p class="estilo-p">Coloque seus dados e venha ver os seus pets</p>
-        </div>
+            <div>
+                <div id="pata" class="d-flex flex-column justify-content-center align-items-center"></div>
+                <h1>Seja bem <br> vindo de volta</h1>
+                <p class="estilo-p">Coloque seus dados e venha ver os seus pets</p>
+            </div>
 
-        <section class="estilo-form">
-            <form action="" >
-                <div class="form-floating mb-3">
-                    <input class="form-control input" id="floatingInput" placeholder="name@example.com" required type="email">
-                    <label for="floatingInput">E-mail</label>
-                </div>
-                <br>
+            <section class="estilo-form">
+                <form action="" >
+                    <div class="form-floating mb-3">
+                        <input class="form-control input" id="floatingInput" placeholder="name@example.com" required type="email">
+                        <label for="floatingInput">E-mail</label>
+                    </div>
+                    <br>
 
-                <div class="form-floating">
-                    <input  class="form-control input" id="floatingPassword" placeholder="Password" required type="password">
-                    <label for="floatingPassword">Senha</label>
-                </div>
+                    <div class="form-floating">
+                        <input  class="form-control input" id="floatingPassword" placeholder="Password" required type="password">
+                        <label for="floatingPassword">Senha</label>
+                    </div>
 
-                
-                    <div class="estilo-cadeado">
-                        <div class="esqueci-senha">
-                            <a data-bs-toggle="modal" data-bs-target="#exampleModal" href="" rel="" >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
-                                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
-                            </svg>
-                            Esqueci a senha</a>
-                        </div>
+                    
+                        <div class="estilo-cadeado">
+                            <div class="esqueci-senha">
+                                <a data-bs-toggle="modal" data-bs-target="#exampleModal" href="" rel="" >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
+                                    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
+                                </svg>
+                                Esqueci a senha</a>
+                            </div>
                         
                         <div class="hero">
                             
@@ -76,14 +76,14 @@
                     </div>
             
 
-                <br>
+                    <br>
 
-                <div>
-                    <button class="btn btn-primary btn-lg botao">Entrar</button>
-                    <p class="tag-style">Não tem uma conta? <a href="cadastro.php">Crie uma conta de graça</a></p>
-                </div>
-            </form>
-        </section> 
+                    <div>
+                        <button class="btn btn-primary btn-lg botao">Entrar</button>
+                        <p class="tag-style">Não tem uma conta? <a href="cadastro.php">Crie uma conta de graça</a></p>
+                    </div>
+                </form>
+            </section> 
 
         </div>
    
