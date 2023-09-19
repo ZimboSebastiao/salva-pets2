b5f8cb7629ba1290616bee6fcae5a1c7775b5e88
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-09-19 11:55:27 -0300

    Footer

---
+++ includes/footer.php
@@ -0,0 +1,35 @@
+<link rel="stylesheet" href="../assets/css/styles.css">
+<footer>
+    <img src="../icones/pet1.png" alt="..." height="35">
+ 
+<div class="lis">
+    <ul>
+        <h3>SalvaPets</h3>
+        <p>não compre, adote!</p>
+        <p>contato.salvapets@gmail.com</p>
+</ul>
+
+    <ul>
+        <h3>Nossos pets</h3>
+        <p>Cachorros</p>
+        <p>Gatos</p>
+</ul>
+    <ul>
+        <h3> Institucional</h3>
+        <p>Sobre Nós</p>
+        <p>Contato</p>
+</ul>
+    <ul>
+        <h3>Centro de Ajuda</h3>
+        <p>Política de Privacidade</p>
+        <p>Ajuda</p>
+
+</ul>
+<div class="copyright">
+    <p>&copy; Copyright 2023 SalvaPets. Todos direitos Reservados.</p>
+    <div>
+    <p>ahsfajsfasfas</p>
+    </div>
+</div>
+</div>
+</footer>
