0d7bc5f00fcd4fed63dbd7b12c2ed781f6bf992c
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-20 10:25:21 -0300

    link do linktree

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>SalvaPets - Home</title>
+  <title>SalvaPets - Adote o seu pet</title>
   <!-- ======== CSS Bootstrap ======== -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   <link rel="stylesheet" href="assets/css/styles.css">
