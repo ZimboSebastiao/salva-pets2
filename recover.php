0b450ed71ff53730174c83a3a06267c83a83e222
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-26 10:31:59 -0300

    Página recover password

--- recover.php
+++ recover.php
@@ -8,6 +8,9 @@
   <!-- ======== CSS Bootstrap ======== -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   <link rel="stylesheet" href="assets/css/styles.css">
+  <style>
+    body {background-color: #fff6f1;}
+  </style>
 </head>
 
 <body>
