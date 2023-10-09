c5801dfdadea5cc6b17f3240ef3bf43cff56e904
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-10-09 19:03:09 -0300

    Página Nossos Pets - Tela desktop

--- nossos-pets.php
+++ nossos-pets.php
@@ -113,13 +113,20 @@
                   $sexo = $pet['sexo'];
                   $imagem = $pet['imagem'];
     
-                  // Corrija a URL da imagem
+                  
                   $imagemUrl = "https://salvapets.onrender.com" . $imagem; ?>
     
         <div class="card border card-item" style="width: 25%;">
           <img src='<?=$imagemUrl?>' class="card-img-top" alt='<?=$nome?>' height="290">
           <div class="card-body">
-            <h5 class="card-title nome-pets"><?=$nome?></h5>
+            <div class="favoritar-nome">
+              <h5 class="card-title nome-pets"><?=$nome?></h5>
+              <p>
+              <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+              </svg>
+              </p>
+            </div>
             <p class="card-text loc-pets"><?=$localizacao?></p>
             <div class="idade-sexo">
               <p class="card-text sexo-pets">
@@ -150,7 +157,7 @@
                 ?>
               <p class="card-text"><?=$anos?> anos e <?=$meses?> meses</p>
             </div>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
           </div>
         </div>
     
