a85536e1b1cbe8f9c8bf1c4b66609b961ddecf02
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-07 21:45:36 -0300

    Cards de pets relacionados

--- detalhe.php
+++ detalhe.php
@@ -298,23 +298,22 @@
   
   <h3 style=" color: #1E266D;">Sobre o Pet</h3>
   <p><?=$sobre?></p>
-  <h3 style=" color: #1E266D;">Pets relacionados</h3>
-</div>
+  <h3 style=" color: #1E266D; padding-top: 30px; padding-bottom: 16px;">Pets relacionados</h3>
+ 
 
-
-
+  
 <?php
-// ... Seu código para obter dados da API ...
+
 $apiUrl = "https://salvapets.onrender.com/pets/";  
 $apiData = file_get_contents($apiUrl);
 $data = json_decode($apiData, true);
-// Embaralhar os dados para exibição aleatória
+
 shuffle($data);
 
 // Exibir apenas 3 cards aleatórios
 ?>
     
-    <div class="container">
+  <div>
     <div class="row">
     <?php
       for ($i = 0; $i < 3; $i++) {
@@ -339,7 +338,11 @@ shuffle($data);
           <div class="favoritar-nome">
             <h5 class="card-title nome-pets"><?=$nome?></h5>
             <p class="favorito">
-              <a class="bi bi-heart" href="?favoritar=<?=$pet['id']?>" ></a>
+              <a href="?favoritar=<?=$pet['id']?>" >
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+              </svg>
+            </a>
             </p>
           </div>
           <p class="card-text loc-pets"><?=$localizacao?></p>
@@ -377,8 +380,13 @@ shuffle($data);
     </div>
     </div>
  <?php } ?>
-    </div>
-    </div>
+  </div>
+  </div>
+
+</div>
+
+
+
 
 
 
