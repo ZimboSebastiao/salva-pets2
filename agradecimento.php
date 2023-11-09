f8e399ef8561d375c8de37ebe50df735a8582f31
kaue-S <kauesilva38@hotmail.com>
2023-11-09 01:03:37 -0300

    Update agradecimento.php

--- agradecimento.php
+++ agradecimento.php
@@ -6,21 +6,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <title>Obrigado</title>
-    <style>
-        /* .card{
-            position: relative;
-            top: 50%;
-            background-color: red;
-        }
-
-        .card{
-            position: absolute;
-            top: 40%;
-            left: 45%;
-            background-color: red;
-        } */
-
-    </style>
 </head>
 <body class="d-flex justify-content-center align-items-center">
         
